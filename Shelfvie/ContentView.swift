0b7221b0c32470c5fa31474c73bc717d261b80ac
saanvikakde <skakde2@asu.edu>
2025-12-17 15:41:24 -0500

    Initial iOS project setup for Shelfvie

---
+++ Shelfvie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shelfvie
+//
+//  Created by Saanvi Kakde on 12/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shelfvie/ShelfvieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelfvieApp.swift
+//  Shelfvie
+//
+//  Created by Saanvi Kakde on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ShelfvieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
